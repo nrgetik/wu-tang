6614d9ff377878b775e739c66265d7b7b4d1cf7d
Thomas Gordon <nrgetik@users.noreply.github.com>
2016-07-07 16:01:50 -0400

    Insert test data

--- db_load.py
+++ db_load.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from wu import Base, Locale, Observation
 
 
 def main():
     engine = create_engine("sqlite:///wu-tang.db")
     Base.metadata.create_all(engine)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    session.add(Locale(airport_icao="KJFK", city="New York", state="NY",
+                       time_zone="EDT"))
+    session.commit()
+
+    # session.add(Observation())
+    # session.commit()
 
 if __name__ == "__main__":
     main()
