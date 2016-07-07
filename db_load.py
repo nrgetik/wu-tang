2a355cb6cb816a19b4c898c0816fee1b5b87247a
Thomas Gordon <nrgetik@users.noreply.github.com>
2016-07-07 15:24:15 -0400

    Don't need declarative_base

--- db_load.py
+++ db_load.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from wu import Base, Locale, Observation
 
