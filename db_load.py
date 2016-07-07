9bc5e7095de0d8601b4697f3d12d45c243b10960
Thomas Gordon <nrgetik@users.noreply.github.com>
2016-07-07 15:10:15 -0400

    Define main

--- db_load.py
+++ db_load.py
@@ -5,6 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from wu import Base, Locale, Observation
 
-engine = create_engine("sqlite:///wu-tang.db")
 
-Base.metadata.create_all(engine)
+def main():
+    engine = create_engine("sqlite:///wu-tang.db")
+    Base.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    main()
