1a3fe2719c0fdc7a3a6bca10312ff501197ed3eb
Thomas Gordon <nrgetik@users.noreply.github.com>
2016-07-11 10:23:21 -0400

    Only grab most recent complete years, skip days with no data

--- wu.sh
+++ wu.sh
@@ -7,22 +7,29 @@ YEARS=${1:-1}
 
 for airport in `cat airports.txt`
 do
-    mkdir -pv csv/$airport
+    mkdir -pv wg-csv/$airport
     for dt in `python -c "from datetime import datetime, timedelta; \
-        now = datetime.now(); \
-        print '\n'.join(((now - timedelta(days=day)).strftime('%Y-%m-%d')) \
-        for day in range((now - datetime(now.year - $YEARS, now.month, \
-            now.day)).days, 0, -1))"`
+        end = datetime(datetime.now().year, 1, 1); \
+        print '\n'.join(((end - timedelta(days=day)).strftime('%Y-%m-%d')) \
+        for day in range((end - datetime(end.year - $YEARS, end.month, \
+            end.day)).days, 0, -1))"`
     do
         URL=`echo $airport $dt \
             | sed 's/\-/ /g' \
             | awk '{printf("https://www.wunderground.com/history/airport/%s/%d/%d/%d/DailyHistory.html?format=1\n", $1, $2, $3, $4)}'`
         echo -n "$airport on $dt... "
-        if [[ ! -f csv/$airport/$dt.csv ]]
+        if [[ ! -f wg-csv/$airport/$dt.csv ]]
         then
-            sleep 0.25
-            curl -s $URL | sed '/./,$!d' | sed 's/<br \/>$//g' > csv/$airport/$dt.csv
-            echo "Done"
+            sleep 0.125
+            curl -s $URL | sed '/./,$!d' | sed 's/<br \/>$//g' > wg-csv/$airport/$dt.csv
+            if grep -q "No daily or hourly history data available" \
+                wg-csv/$airport/$dt.csv
+            then
+                rm -f wg-csv/$airport/$dt.csv
+                echo "Omitted (bad/no data)"
+            else
+                echo "Done"
+            fi
         else
             echo "Skipped"
         fi
