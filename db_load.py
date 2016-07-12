b7b15dc78a26d5cb0c24bc6ce7d8e6c0a9592f64
Thomas Gordon <nrgetik@users.noreply.github.com>
2016-07-12 16:06:44 -0400

    Load all available data, add a heat index value as well

--- db_load.py
+++ db_load.py
@@ -4,12 +4,37 @@
 import csv
 import os
 from datetime import datetime
+from math import exp
+from math import sqrt
 from sqlalchemy import create_engine
 
 from wu import Base, Locale, Observation
 
 
-def validate(v, t):
+def calc_relative_humidity(t, dp):
+    return 100 * (exp((17.625 * dp) / (243.04 + dp)) /
+                  exp((17.625 * t) / (243.04 + t)))
+
+
+def calc_heat_index(t, rh):
+    simple = 0.5 * (t + 61.0 + ((t - 68.0) * 1.2) + (rh * 0.094))
+    if ((t + simple) / 2.0) < 80.0:
+        return simple
+    else:
+        full = -42.379 + (2.04901523 * t) + (10.14333127 * rh) - \
+            (0.22475541 * t * rh) - (0.00683783 * t * t) - \
+            (0.05481717 * rh * rh) + (0.00122874 * t * t * rh) + \
+            (0.00085282 * t * rh * rh) - (0.00000199 * t * t * rh * rh)
+        if (rh < 13.0) and (80.0 <= t <= 112.0):
+            return full - ((13.0 - rh) / 4.0) * \
+                sqrt((17.0 - abs(t - 95.0)) / 17.0)
+        elif (rh > 85.0) and (80.0 <= t <= 87.0):
+            return full + ((rh - 85.0) / 10.0) * ((87.0 - t) / 5.0)
+        else:
+            return full
+
+
+def validate_field(v, t):
     if t == float:
         try:
             return float(v)
@@ -44,23 +69,34 @@ def main():
                 for row in reader:
                     loc_obsvns.append([" ".join((date_local, row[0]))] +
                                       [col for col in row[1:]])
+        for obs in loc_obsvns:
+            obs[1] = validate_field(obs[1], float)
+            obs[2] = validate_field(obs[2], float)
+            obs[3] = validate_field(obs[3], float)
+            if obs[1] and obs[2] and not obs[3]:
+                obs[3] = calc_relative_humidity(obs[1], obs[2])
+            if obs[1] and obs[3]:
+                obs.append(calc_heat_index(obs[1], obs[3]))
+            else:
+                obs.append(None)
         engine.execute(
             Observation.__table__.insert(),
             [{"datetime_local": datetime.strptime(row[0], "%Y-%m-%d %I:%M %p"),
-              "time_zone": validate(time_zone, str),
-              "temperature_f": validate(row[1], float),
-              # "dew_point_f": validate(row[2], float),
-              "relative_humidity": validate(row[3], float),
-              # "sea_level_pressure_in": validate(row[4], float),
-              "visibility_miles": validate(row[5], float),
-              # "wind_direction": validate(row[6], str),
-              "wind_speed_mph": validate(row[7], float),
-              "gust_speed_mph": validate(row[8], float),
-              "precipitation_in": validate(row[9], float),
-              "events": validate(row[10], str),
-              "conditions": validate(row[11], str),
-              # "wind_dir_degrees": validate(row[12], float),
-              # "datetime_utc": datetime.strptime(row[13], "%Y-%m-%d %H:%M:%S"),
+              "time_zone": validate_field(time_zone, str),
+              "temperature_f": round(row[1], 2),
+              "dew_point_f": round(row[2], 2),
+              "relative_humidity": round(row[3], 2),
+              "heat_index": round(row[14], 2),
+              "sea_level_pressure_in": round(validate_field(row[4], float), 2),
+              "visibility_miles": round(validate_field(row[5], float), 2),
+              "wind_direction": validate_field(row[6], str),
+              "wind_speed_mph": round(validate_field(row[7], float), 2),
+              "gust_speed_mph": round(validate_field(row[8], float), 2),
+              "precipitation_in": round(validate_field(row[9], float), 2),
+              "events": validate_field(row[10], str),
+              "conditions": validate_field(row[11], str),
+              "wind_dir_degrees": round(validate_field(row[12], float), 2),
+              "datetime_utc": datetime.strptime(row[13], "%Y-%m-%d %H:%M:%S"),
               "locale_airport_icao": airport_icao}
              for row in loc_obsvns]
         )
