1aa2424800308214f5bf1b7228fed93fed82c058
Thomas Gordon <nrgetik@users.noreply.github.com>
2016-07-13 12:37:29 -0400

    Parameter update/fixed syntax errors

--- analysis.sql
+++ analysis.sql
@@ -1,10 +1,15 @@
+DROP VIEW filtered;
+DROP VIEW total;
+DROP VIEW final;
+
 CREATE VIEW filtered AS
 SELECT locale_airport_icao, date(datetime_local) AS day, count(*) AS num
 FROM observations
 WHERE time(datetime_local) BETWEEN time('12:00:00') AND time('18:00:00') AND
 (temperature_f IS NOT NULL AND temperature_f >= 50.0) AND (heat_index_f IS NOT
-    NULL AND heat_index_f < 91.0) AND conditions IN ('Clear') AND
-(precipitation_in <= 0.1 OR precipitation_in IS NULL) AND events IS NULL
+    NULL AND heat_index_f < 91.0) AND conditions IN ('Clear', 'Scattered
+    Clouds') AND (precipitation_in <= 0.1 OR precipitation_in IS NULL) AND
+events IS NULL
 GROUP BY locale_airport_icao, day
 ORDER BY locale_airport_icao, day DESC;
 
@@ -22,11 +27,13 @@ SELECT filtered.locale_airport_icao, filtered.day, (cast(filtered.num AS float)
     / cast(total.num AS float)) AS percent
 FROM filtered, total
 WHERE filtered.locale_airport_icao = total.locale_airport_icao AND filtered.day
-= total.day AND percent >= cast((2.0 / 3.0) AS float);
+= total.day AND percent >= cast((2.0 / 3.0) AS float)
 GROUP BY filtered.locale_airport_icao, filtered.day
 ORDER BY filtered.locale_airport_icao, filtered.day DESC;
 
-SELECT locale_airport_icao, count(*) AS days
-FROM final
-GROUP BY locale_airport_icao
+SELECT final.locale_airport_icao, count(*) AS days, locales.city,
+locales.state
+FROM final, locales
+WHERE final.locale_airport_icao = locales.airport_icao
+GROUP BY final.locale_airport_icao
 ORDER BY days DESC;
