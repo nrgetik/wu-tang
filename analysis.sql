175ed2b486f74eb6f13236bf55263dc969e6f051
Thomas Gordon <nrgetik@users.noreply.github.com>
2016-07-13 14:51:07 -0400

    Just temperature/humidity

--- analysis.sql
+++ analysis.sql
@@ -7,8 +7,7 @@ SELECT locale_airport_icao, date(datetime_local) AS day, count(*) AS num
 FROM observations
 WHERE time(datetime_local) BETWEEN time('12:00:00') AND time('18:00:00') AND
 (temperature_f IS NOT NULL AND temperature_f >= 55.0) AND (heat_index_f IS NOT
-    NULL AND heat_index_f < 85.0) AND conditions IN ('Clear', 'Scattered
-    Clouds', 'Partly Cloudy') AND events IS NULL
+    NULL AND heat_index_f < 85.0)
 GROUP BY locale_airport_icao, day
 ORDER BY locale_airport_icao, day DESC;
 
@@ -26,7 +25,7 @@ SELECT filtered.locale_airport_icao, filtered.day, (cast(filtered.num AS float)
     / cast(total.num AS float)) AS percent
 FROM filtered, total
 WHERE filtered.locale_airport_icao = total.locale_airport_icao AND filtered.day
-= total.day AND percent >= cast((2.0 / 3.0) AS float)
+= total.day AND percent >= cast((6.0 / 6.0) AS float)
 GROUP BY filtered.locale_airport_icao, filtered.day
 ORDER BY filtered.locale_airport_icao, filtered.day DESC;
 
